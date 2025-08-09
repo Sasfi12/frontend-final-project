8edee5215fe5b2f7db262553dc483240e9c990f0
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-08-09 10:24:20 +0200

    removed comments

--- src/lib/apiTypes.ts
+++ src/lib/apiTypes.ts
@@ -16,21 +16,3 @@ export interface Book {
     Quote2 : string, 
     Quote3 : string
 }
-/* 
-id	146
-title	"All Quiet on the Western Front"
-authors	"Erich Maria Remarque"
-description	"This is the testament of Paul Bäumer, who enlists with his classmates in the German army of World War I. These young men bec…its young men of the same generation but different uniforms against one another – if only he can come out of the war alive."
-edition	""
-format	"Mass Market Paperback"
-num_pages	296
-rating	3.94
-rating_count	306844
-review_count	8164
-genres	"Classics, Fiction, Historical, Historical Fiction, War, Literature"
-genre_list	"Classics,Fiction,Historical,Historical Fiction,War,Literature"
-image_url	"https://images.gr-assets.com/books/1441227765l/355697.jpg"
-Quote1	"“You don’t forget the face of the person who was your last hope.”"
-Quote2	"“Remember, we're madly in love, so it's all right to kiss me anytime you feel like it.”"
-Quote3	"“May the odds be ever in your favor!”"
-*/

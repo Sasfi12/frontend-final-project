2659934e9aa195aac34ea205780f288d08856397
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-08-09 05:55:13 +0200

    feat: added type for DarkModeSlice

--- src/lib/features/darkmode/DarkModeSlice.ts
+++ src/lib/features/darkmode/DarkModeSlice.ts
@@ -1,12 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit"
-const initialState = {
+interface DarmodeState {
+    toggle : boolean
+}
+const initialState : DarmodeState = {
     toggle: false
 }
 const DarkModeSlice = createSlice( {
     name: "darkmode",
     initialState,
     reducers : {
-        darkModeClick : (state) => {
+        darkModeClick : (state : DarmodeState) => {
             state.toggle = !state.toggle
             console.log(state.toggle)
         } 

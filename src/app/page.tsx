3a979d3435e4b5bc322648371e35b68a6be7299d
Sasfi12 <sufyan10.muhammad1070@hotmail.com>
2025-08-10 11:37:53 +0200

    feat: applied my types to my main page, I see no red

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,17 @@ import { DataProvider } from "./data-provider";
 import Carousel from "./components/carousel/Carousel";
 import Featured from "./components/featured/Features";
 import { useSelector } from "react-redux";
+import { RootState } from "@/lib/store";
+import { Book } from "@/lib/apiTypes";
 export default function Home() {
   const data = useContext(DataProvider).data
-  const dark = useSelector((state) => state.darkmode.toggle)
+  const dark = useSelector<RootState>((state) => state.darkmode.toggle)
   // randomized data for the carousel. 
   const randomize = () => {
     return data[Math.floor(Math.random() * data.length )] 
   }
   const allWellRated = () => {
-    let array = [] ;
+    let array : Book[] = [] ;
     data.forEach(e => {
       if(e.rating >= 4.5) {
         array = [...array , e]
@@ -23,7 +25,7 @@ export default function Home() {
   }
   const recommendedId = () => {
     const recommended = [29 ,44 , 45 , 81 , 108 , 167 , 38 ,222 , 240] 
-    let array = [] ;
+    let array : Book[] = [] ;
     data.forEach(e => {
       if (recommended.includes(e.id)) {
         array = [...array , e]
